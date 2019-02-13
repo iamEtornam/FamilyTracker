8b6d18e7aed456c0b2ff8d42bd0857960ac9f4f1
regnex <sunumacbright@gmail.com>
2019-02-13 14:29:16 +0000

    feat: retrieve data from settings preference

--- app/src/main/java/co/etornam/familytracker/ui/MainActivity.java
+++ app/src/main/java/co/etornam/familytracker/ui/MainActivity.java
@@ -1,15 +1,26 @@
 package co.etornam.familytracker.ui;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 import co.etornam.familytracker.R;
 
 public class MainActivity extends AppCompatActivity {
+	boolean isTrackingActivated;
+	boolean isPinActivated;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		setupPreferences();
+	}
+
+	private void setupPreferences() {
+		SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+		isTrackingActivated = sharedPreferences.getBoolean("track_key", false);
+		isPinActivated = sharedPreferences.getBoolean("protect_key", false);
 	}
 }
