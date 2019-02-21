45f18b2a400e568e778bdde8738c7a5c1c5c5b80
regnex <sunumacbright@gmail.com>
2019-02-21 13:47:26 +0000

    feat: added database persistence

--- app/src/main/java/co/etornam/familytracker/FamilyTracker.java
+++ app/src/main/java/co/etornam/familytracker/FamilyTracker.java
@@ -2,11 +2,14 @@ package co.etornam.familytracker;
 
 import android.app.Application;
 
+import com.google.firebase.database.FirebaseDatabase;
+
 public class FamilyTracker extends Application {
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
+		FirebaseDatabase.getInstance().setPersistenceEnabled(true);
 	}
 
 
