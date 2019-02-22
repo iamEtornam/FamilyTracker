a3e614481f2f4943cf0bd4b2f64afa3790136706
regnex <sunumacbright@gmail.com>
2019-02-22 16:41:42 +0000

    feat: added Tracking constant

--- app/src/main/java/co/etornam/familytracker/util/Constants.java
+++ app/src/main/java/co/etornam/familytracker/util/Constants.java
@@ -4,6 +4,7 @@ public class Constants {
 	public static String USER_DB = "users";
 	public static String CONTACT_DB = "contacts";
 	public static String HEALTH_DB = "health";
+	public static String TRACKING_DB = "tracking";
 	public static final String DOCTOR_NAME = "DOCTOR_NAME";
 	public static final String DOCTOR_NUMBER = "DOCTOR_NUMBER";
 }
