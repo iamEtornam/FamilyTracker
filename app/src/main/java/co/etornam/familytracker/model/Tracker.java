c5d0cbfc307e0753087ae924d1ced24bd6597011
regnex <sunumacbright@gmail.com>
2019-02-22 16:37:14 +0000

    feat: added Tracker POJO

---
+++ app/src/main/java/co/etornam/familytracker/model/Tracker.java
@@ -0,0 +1,25 @@
+package co.etornam.familytracker.model;
+
+public class Tracker {
+	private String longitude;
+	private String latitude;
+
+	public Tracker() {
+	}
+
+	public String getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+
+	public String getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+}
