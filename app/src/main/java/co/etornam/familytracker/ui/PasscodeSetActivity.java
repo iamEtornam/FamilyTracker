687a3b632f0cbf1fb334eef64fdde37f0e944a08
Etornam Sunu <sunumacbright@gmail.com>
2020-03-23 09:21:57 +0000

    Update PasscodeSetActivity.java

--- app/src/main/java/co/etornam/familytracker/ui/PasscodeSetActivity.java
+++ app/src/main/java/co/etornam/familytracker/ui/PasscodeSetActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
